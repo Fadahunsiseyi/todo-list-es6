6fc5e865c1204210d1f37e0548bd001e2e526227
FADAHUNSI <oluwaseyisamuel018@gmail.com>
2022-07-07 00:32:57 +0100

    Fixing linters error

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import "./style.css";
+import './style.css';
 
-const taskContainer = document.querySelector(".todo-lists");
+const taskContainer = document.querySelector('.todo-lists');
 
 const todoTasks = [
   {
-    description: "Wash the dishes",
+    desc: 'Wash the dishes',
     completed: false,
     index: 0,
   },
   {
-    description: "Complete To Do list project",
+    desc: 'Complete To Do list project',
     completed: false,
     index: 1,
   },
   {
-    description: "Fix car",
+    desc: 'Fix car',
     completed: false,
     index: 2,
   },
 ];
 
-todoTasks.map((task, i) => {
-  const markup = `<li class="todo-list border-bottom">${task.description}</li>`;
-  taskContainer.insertAdjacentHTML("beforeend", markup);
+todoTasks.forEach((task) => {
+  const markup = `<li class="todo-list border-bottom">${task.desc}</li>`;
+  taskContainer.insertAdjacentHTML('beforeend', markup);
 });

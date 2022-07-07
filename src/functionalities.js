5cb2f61a3f8dba9cf338dfcd57edeae13489766c
FADAHUNSI <oluwaseyisamuel018@gmail.com>
2022-07-07 15:42:51 +0100

    Add todo to the lists items

--- src/functionalities.js
+++ src/functionalities.js
@@ -3,12 +3,39 @@
 const todoInputContainer = document.querySelector('.todo-input-container')
 const todoInput = document.querySelector('.todo-input')
 const todoStorage = JSON.parse(localStorage.getItem('todos')) || []
+const todoLists = document.querySelector('.todo-lists')
 console.log(todoStorage, 'here!!!')
+
+const displayTodo = () => {
+    console.log('i have been displayed')
+    if(!todoStorage) return;
+    const arrangeTodos = todoStorage.sort((a, b) => a.index - b.index)
+    console.log(arrangeTodos, 'arrange me')
+    todoLists.innerHTML = ''
+    arrangeTodos.forEach((todo) => {
+        const theTodo =  `
+        <li class="list list-${todo.index}">
+          <span class="list-checks">
+            <button class="btn check" data-btn="${todo.index}">
+              <span class="empty-check active"><i class="fa-solid fa-square active"></i></span>
+              <span class="checked"><i class="fa-solid fa-check"></i></span>
+            </button>
+            <input type="text" class="todo" value="${todo.desc}"/>
+          </span>
+          <button class="btn remove" data-remove="${todo.index}">
+            <i class="fa-solid fa-trash-can"></i>
+          </button>
+        </>
+      `;
+      todoLists.insertAdjacentHTML('beforeend', theTodo)
+    })
+}
 
 todoInputContainer.addEventListener('submit', (e) => {
     console.log('clicked me!!')
     e.preventDefault()
     const todoInputValue = todoInput.value.trim()
+    todoInput.value = ''
     if(!todoInputValue) return;
     const todoList = {
         desc: todoInputValue,
@@ -17,4 +44,5 @@ todoInputContainer.addEventListener('submit', (e) => {
     }
     todoStorage.push(todoList)
     localStorage.setItem('todos',JSON.stringify(todoStorage))
+    displayTodo()
 })
--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ import '@fortawesome/fontawesome-free/js/solid';
 
 const taskContainer = document.querySelector('.todo-lists');
 
-const todoTasks = [
-  {
-    desc: 'Wash the dishes',
-    completed: false,
-    index: 0,
-  },
-  {
-    desc: 'Complete To Do list project',
-    completed: false,
-    index: 1,
-  },
-  {
-    desc: 'Fix car',
-    completed: false,
-    index: 2,
-  },
-];
+// const todoTasks = [
+//   {
+//     desc: 'Wash the dishes',
+//     completed: false,
+//     index: 0,
+//   },
+//   {
+//     desc: 'Complete To Do list project',
+//     completed: false,
+//     index: 1,
+//   },
+//   {
+//     desc: 'Fix car',
+//     completed: false,
+//     index: 2,
+//   },
+// ];
 
-todoTasks.forEach((task) => {
-  const markup = `<li class="todo-list border-bottom">${task.desc}</li>`;
-  taskContainer.insertAdjacentHTML('beforeend', markup);
-});
+// todoTasks.forEach((task) => {
+//   const markup = `<li class="todo-list border-bottom">${task.desc}</li>`;
+//   taskContainer.insertAdjacentHTML('beforeend', markup);
+// });

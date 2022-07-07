c2f6acb706059f5e40e3c95872e091ef614e5f9c
FADAHUNSI <oluwaseyisamuel018@gmail.com>
2022-07-07 13:49:45 +0100

    Add logic to store value to localstorage

--- src/functionalities.js
+++ src/functionalities.js
@@ -1,3 +1,20 @@
 
-const container = document.querySelector('p')
-container.classList.add('para')
+
+const todoInputContainer = document.querySelector('.todo-input-container')
+const todoInput = document.querySelector('.todo-input')
+const todoStorage = JSON.parse(localStorage.getItem('todos')) || []
+console.log(todoStorage, 'here!!!')
+
+todoInputContainer.addEventListener('submit', (e) => {
+    console.log('clicked me!!')
+    e.preventDefault()
+    const todoInputValue = todoInput.value.trim()
+    if(!todoInputValue) return;
+    const todoList = {
+        desc: todoInputValue,
+        completed: false,
+        index: todoStorage.length,
+    }
+    todoStorage.push(todoList)
+    localStorage.setItem('todos',JSON.stringify(todoStorage))
+})

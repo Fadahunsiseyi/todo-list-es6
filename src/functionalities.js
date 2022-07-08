ce36163eba322a141e4ccb11d999f0891d4f3c8f
FADAHUNSI <oluwaseyisamuel018@gmail.com>
2022-07-08 08:31:26 +0100

    display filtered todos

--- src/functionalities.js
+++ src/functionalities.js
@@ -1,19 +1,17 @@
-
-
-const todoInputContainer = document.querySelector('.todo-input-container')
-const todoInput = document.querySelector('.todo-input')
-const todoStorage = JSON.parse(localStorage.getItem('todos')) || []
-const todoLists = document.querySelector('.todo-lists')
-console.log(todoStorage, 'here!!!')
+const todoInputContainer = document.querySelector(".todo-input-container");
+const todoInput = document.querySelector(".todo-input");
+const todoStorage = JSON.parse(localStorage.getItem("todos")) || [];
+const todoLists = document.querySelector(".todo-lists");
+console.log(todoStorage, "here!!!");
 
 const displayTodo = () => {
-    console.log('i have been displayed')
-    if(!todoStorage) return;
-    const arrangeTodos = todoStorage.sort((a, b) => a.index - b.index)
-    console.log(arrangeTodos, 'arrange me')
-    todoLists.innerHTML = ''
-    arrangeTodos.forEach((todo) => {
-        const theTodo =  `
+  console.log("i have been displayed");
+  if (!todoStorage) return;
+  const arrangeTodos = todoStorage.sort((a, b) => a.index - b.index);
+  console.log(arrangeTodos, "arrange me");
+  todoLists.innerHTML = "";
+  arrangeTodos.forEach((todo) => {
+    const theTodo = `
         <li class="list border-bottom list-${todo.index}">
           <span class="list-checks">
             <button class="btn check" data-btn="${todo.index}">
@@ -27,51 +25,53 @@ const displayTodo = () => {
           </button>
         </>
       `;
-      todoLists.insertAdjacentHTML('beforeend', theTodo)
-    })
-}
+    todoLists.insertAdjacentHTML("beforeend", theTodo);
+  });
+};
 
-todoInputContainer.addEventListener('submit', (e) => {
-    console.log('clicked me!!')
-    e.preventDefault()
-    const todoInputValue = todoInput.value.trim()
-    todoInput.value = ''
-    if(!todoInputValue) return;
-    const todoList = {
-        desc: todoInputValue,
-        completed: false,
-        index: todoStorage.length,
-    }
-    todoStorage.push(todoList)
-    localStorage.setItem('todos',JSON.stringify(todoStorage))
-    displayTodo()
-})
+todoInputContainer.addEventListener("submit", (e) => {
+  console.log("clicked me!!");
+  e.preventDefault();
+  const todoInputValue = todoInput.value.trim();
+  todoInput.value = "";
+  if (!todoInputValue) return;
+  const todoList = {
+    desc: todoInputValue,
+    completed: false,
+    index: todoStorage.length,
+  };
+  todoStorage.push(todoList);
+  localStorage.setItem("todos", JSON.stringify(todoStorage));
+  displayTodo();
+});
 
-todoLists.addEventListener('click', (e) => {
-  const clicked = e.target.closest('.todo')
-  if(!clicked) return;
-  clicked.addEventListener('keyup', () => {
-    const listNum = +clicked.dataset.desc
-    const findTodo = todoStorage.find(todo => todo.index === listNum)
-    findTodo.desc = clicked.value.trim()
-    localStorage.setItem('todos',JSON.stringify(todoStorage))
-  })
-})
+todoLists.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".todo");
+  if (!clicked) return;
+  clicked.addEventListener("keyup", () => {
+    const listNum = +clicked.dataset.desc;
+    const findTodo = todoStorage.find((todo) => todo.index === listNum);
+    findTodo.desc = clicked.value.trim();
+    localStorage.setItem("todos", JSON.stringify(todoStorage));
+  });
+});
 
-todoLists.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log('been clicked')
-  const clicked = e.target.closest('.remove')
-  if(!clicked) return;
+todoLists.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("been clicked");
+  const clicked = e.target.closest(".remove");
+  if (!clicked) return;
   const listNum = +clicked.dataset.remove;
-   const filteredTodo = todoStorage.filter((todo) => todo.index !== listNum)
-   console.log(filteredTodo, 'filtered')
-   let newTodo = []
-   filteredTodo.forEach((nTodo, i) => {
-     console.log(nTodo, i, 'hope')
-     nTodo.index = i
-     newTodo.push(nTodo)
-   })
-   localStorage.setItem('todos', JSON.stringify(newTodo))
-   displayTodo()
-})
+  const filteredTodo = todoStorage.filter((todo) => todo.index !== listNum);
+  console.log(filteredTodo, "filtered");
+  let newTodo = [];
+  filteredTodo.forEach((nTodo, i) => {
+    console.log(nTodo, i, "hope");
+    nTodo.index = i;
+    newTodo.push(nTodo);
+    console.log(newTodo, "entirely");
+  });
+  e.target.closest(".list").remove();
+  localStorage.setItem("todos", JSON.stringify(newTodo));
+  displayTodo();
+});

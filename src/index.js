c0e52923ec8280f1cf7881c463c001abd4c922e8
FADAHUNSI <oluwaseyisamuel018@gmail.com>
2022-07-08 08:49:35 +0100

    removed console log

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import './style.css';
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
+import "./style.css";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
 
-const todoLists = document.querySelector('.todo-lists')
-const todoStorage = JSON.parse(localStorage.getItem('todos')) || []
+const todoLists = document.querySelector(".todo-lists");
+const todoStorage = JSON.parse(localStorage.getItem("todos")) || [];
 
-
-window.addEventListener('load', (e) => {
-    if(!todoStorage) return;
-    const arrangeTodos = todoStorage.sort((a, b) => a.index - b.index)
-    console.log(arrangeTodos, 'arrange me')
-    todoLists.innerHTML = ''
-    arrangeTodos.forEach((todo) => {
-        const theTodo =  `
+window.addEventListener("load", (e) => {
+  if (!todoStorage) return;
+  const arrangeTodos = todoStorage.sort((a, b) => a.index - b.index);
+  console.log(arrangeTodos, "arrange me");
+  todoLists.innerHTML = "";
+  arrangeTodos.forEach((todo) => {
+    const theTodo = `
         <li class="list border-bottom list-${todo.index}">
           <span class="list-checks">
             <button class="btn check" data-btn="${todo.index}">
@@ -27,7 +26,6 @@ window.addEventListener('load', (e) => {
           </button>
         </>
       `;
-      todoLists.insertAdjacentHTML('beforeend', theTodo)
-    })
-})
-
+    todoLists.insertAdjacentHTML("beforeend", theTodo);
+  });
+});

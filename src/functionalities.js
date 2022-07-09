ac0b2449672a396c084d241c0b5c2f0cc9137e4c
FADAHUNSI <oluwaseyisamuel018@gmail.com>
2022-07-09 16:01:26 +0100

    Add interactive logic

--- src/functionalities.js
+++ src/functionalities.js
@@ -1,21 +1,25 @@
 const todoInputContainer = document.querySelector('.todo-input-container');
 const todoInput = document.querySelector('.todo-input');
-const todoStorage = JSON.parse(localStorage.getItem('todos')) || [];
 const todoLists = document.querySelector('.todo-lists');
 
 const displayTodo = () => {
+  const todoStorage = JSON.parse(localStorage.getItem('todos')) || [];
   if (!todoStorage) return;
   const arrangeTodos = todoStorage.sort((a, b) => a.index - b.index);
   todoLists.innerHTML = '';
   arrangeTodos.forEach((todo) => {
+    const {completed} = todo;
+    const mark = !completed ? '' : 'active'
+    const notMark = !completed ? 'active' : ''
+    const strike = completed ? 'cross' : ''
     const theTodo = `
         <li class='list border-bottom list-${todo.index}'>
           <span class='list-checks'>
             <button class='btn check' data-btn='${todo.index}'>
-              <span class='empty-check active'><i class='fa-solid fa-square active'></i></span>
-              <span class='checked'><i class='fa-solid fa-check'></i></span>
+              <span class='empty-check ${notMark}'><i class='fa-regular fa-square active'></i></span>
+              <span class='checked ${mark}'><i class='fa-solid fa-check'></i></span>
             </button>
-            <input type='text' data-desc=${todo.index} class='todo' value='${todo.desc}'/>
+            <input type='text' data-desc=${todo.index} class='todo ${strike}' value='${todo.desc}'/>
           </span>
           <button class='btn remove' data-remove='${todo.index}'>
             <i class='fa-solid fa-trash-can'></i>
@@ -28,6 +32,7 @@ const displayTodo = () => {
 
 todoInputContainer.addEventListener('submit', (e) => {
   e.preventDefault();
+  const todoStorage = JSON.parse(localStorage.getItem('todos')) || [];
   const todoInputValue = todoInput.value.trim();
   todoInput.value = '';
   if (!todoInputValue) return;
@@ -54,6 +59,7 @@ todoLists.addEventListener('click', (e) => {
 
 todoLists.addEventListener('click', (e) => {
   e.preventDefault();
+  const todoStorage = JSON.parse(localStorage.getItem('todos')) || [];
   const clicked = e.target.closest('.remove');
   if (!clicked) return;
   const listNum = +clicked.dataset.remove;
@@ -65,4 +71,20 @@ todoLists.addEventListener('click', (e) => {
   });
   e.target.closest('.list').remove();
   localStorage.setItem('todos', JSON.stringify(newTodo));
+  displayTodo()
 });
+
+todoLists.addEventListener('click', (e) => {
+  const clicked = e.target.closest('.check')
+  if(!clicked) return;
+  const listNum = +clicked.dataset.btn
+  const listEl = document.querySelector(`.list-${listNum}`)
+  listEl.querySelector('.empty-check').classList.toggle('active');
+  listEl.querySelector('.checked').classList.toggle('active');
+  listEl.querySelector('.todo').classList.toggle('cross');
+  const todoStorage = JSON.parse(localStorage.getItem('todos')) || [];
+
+  const compTask = todoStorage.find((todo) => todo.index === listNum);
+  compTask.completed = !compTask.completed;
+  localStorage.setItem('todos', JSON.stringify(todoStorage));
+})
